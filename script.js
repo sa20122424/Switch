fb58a15476b7d32a1e09553f3d2efe6a559aa47c
sa20122424 <saloxiddin201224@gmail.com>
2024-08-13 15:48:38 +0500

    salom

---
+++ script.js
@@ -0,0 +1,28 @@
+let day = +prompt("son kiriting");
+
+
+switch (day) {
+    case 1:
+        alert('Monday');
+        break;
+    case 2:
+        alert('Tuesday');
+        break;
+    case 3:
+        alert('Wednesday');
+        break;
+    case 4:
+        alert('Thursday');
+        break;
+    case 5:
+        alert('Friday');
+        break;
+    case 6:
+        alert('Saturday');
+        break;
+    case 7:
+       alert('Sunday');
+        break;
+    default:
+        alert('Invalid day');
+}
